0c1629b11dcd8e8d2ad19ed0ad955d7b925eac2d
Peter <fridrex@mail.com>
2025-04-22 17:56:26 +0100

    feat: set up basic file structure for front end

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,47 @@
+const formatDate = (dateString) => {
+
+};
+
+const formatCurrency = (amount) => {
+
+};
+
+const calculateRemainingTime = (endDate) => {
+
+};
+
+const copyToClipboard = (text) => {
+
+};
+
+const checkModulesComplete = (sessionId) => {
+
+};
+
+const getSessionId = () => {
+
+};
+
+const getAuthToken = () => {
+
+};
+
+const validateWalletAddress = (address) => {
+
+};
+
+const validateTransactionAmount = (amount) => {
+
+};
+
+module.exports = {
+  formatDate,
+  formatCurrency,
+  calculateRemainingTime,
+  copyToClipboard,
+  checkModulesComplete,
+  getSessionId,
+  getAuthToken,
+  validateWalletAddress,
+  validateTransactionAmount
+};
