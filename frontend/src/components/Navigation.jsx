6006a69fc6e126bffd5f4b6c2754683b2057c5e8
Peter <fridrex@mail.com>
2025-04-23 18:41:32 +0100

    feat: implement navigation component for desktops and tablets

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Navigation = () => {
@@ -11,20 +11,32 @@ const Navigation = () => {
 
     return (
         <>
+            <div className="navigation">
             <div className="logo">
                 <Link to="/"><img src="src/assets/images/logo.png" alt="Logo" className="logo__img"/></Link>
                 <p>Blockchain Education</p>
             </div>
-            <nav className={`navigation ${isMenuOpen ? "open" : ""}`}>
-                <ul className={`navigation__links ${isMenuOpen ? "open" : ""}`}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/learn">Learn</Link></li>
-                    <li><Link to="/wallet">Wallet</Link></li>
-                    <li><Link to="/resources">Resources</Link></li>
-                    <li><Link to="/about">About</Link></li>
+            <nav className={`navigation__menu ${isMenuOpen ? "open" : ""}`}>
+                <ul className={`navigation__menu__links ${isMenuOpen ? "open" : ""}`}>
+                    <li>
+                        <NavLink
+                        to="/"
+                        end
+                        // className={({ isActive }) => {
+                        //     console.log("Home isActive:", isActive);
+                        //     isActive ? 'navigation__menu__links__link--active' : ''}}
+                        style={({ isActive }) => isActive ? { color: 'var(--secondary-accent)' } : {}}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li><NavLink to="/learn" style={({ isActive }) => isActive ? { color: 'var(--secondary-accent)' } : {}}>Learn</NavLink></li>
+                    <li><NavLink to="/wallet" style={({ isActive }) => isActive ? { color: 'var(--secondary-accent)' } : {}}>Wallet</NavLink></li>
+                    <li><NavLink to="/resources" style={({ isActive }) => isActive ? { color: 'var(--secondary-accent)' } : {}}>Resources</NavLink></li>
+                    <li><NavLink to="/about" style={({ isActive }) => isActive ? { color: 'var(--secondary-accent)' } : {}}>About</NavLink></li>
                 </ul>
                 <HamburgerMenu isOpen={isMenuOpen} onClick={toggleMenu} />
             </nav>
+            </div>
         </>
     )
 };
