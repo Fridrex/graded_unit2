b4ed6b2880c204a6d3e1ae8d3957e370edf4d82e
Peter <fridrex@mail.com>
2025-04-26 19:14:50 +0100

    feat: implement responsive create wallet part

--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -9,6 +9,18 @@ const Wallet = () => {
                 <div className="wallet__header">
                     <h1>Experience Digital Currency Management in a Safe Environment</h1>
                 </div>
+                <div className="wallet__create-wallet">
+                    <h2>Create New Wallet</h2>
+                    <div className="wallet__create-wallet__content">
+                        <div className="wallet__create-wallet__content__text-and-button">
+                        <p>Ready to see how digital currency wallets work? Our simulator lets you experience the essential features of a cryptocurrency wallet without any financial risk. Create your wallet to understand seed phrases, public addresses, and basic transactions.</p>
+                        <button className="create-wallet__button">Create Your First Wallet</button>
+                        </div>
+                        <div className="wallet__create-wallet__content__image">
+                            <img src="src/assets/images/bitcoin-key-icon.png" alt="Bitcoin Key" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
